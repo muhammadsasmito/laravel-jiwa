a846a9e16ba090e4050639688fbf3be2c9628159
unknown <muhammad.sasmito.a.w@gmail.com>
2017-11-03 14:55:23 +0700

    tambah sql

--- app/Admin.php
+++ app/Admin.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Foundation\Auth\Admin as Authenticatable;
 
 class Admin extends Authenticatable
 {
