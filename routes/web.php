5755eacec215fd52214c2c91cc1ec1301a7903d0
unknown <muhammad.sasmito.a.w@gmail.com>
2017-11-07 09:02:35 +0700

    add admin method

--- routes/web.php
+++ routes/web.php
@@ -38,15 +38,16 @@ Route::post('pesan/kirim', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
 
-Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+// Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+// Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 
 Route::prefix('admin')->group(function() {
+    Route::get('/', 'AdminController@index')->name('admin.dashboard');
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-    Route::get('/', 'AdminController@index')->name('admin.dashboard');
   });
 
