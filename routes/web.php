3160b5be9285909c15e498021a7f88c0153da10b
muhammadsasmito <muhammadsasmitoaw@gmail.com>
2017-11-14 09:00:38 +0700

    simple crud laravel

--- routes/web.php
+++ routes/web.php
@@ -41,11 +41,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/users/logout', 'Auth\LoginController@userLogout')->name('user.logout');
 
-// Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
-// Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-// Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
-
 Route::prefix('admin')->group(function() {
+    Route::resource('manageadmins', 'ManageAdminController');
     Route::get('/', 'AdminController@index')->name('admin.dashboard');
     Route::get('/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
     Route::post('/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
